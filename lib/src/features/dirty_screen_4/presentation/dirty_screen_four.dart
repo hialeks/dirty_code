ecf07279c67c64099acde16723b94b79bc255a90
Aleks <auzunov@gmail.com>
2024-05-21 15:45:22 +0200

    Aufgabe Fertig

--- lib/src/features/dirty_screen_4/presentation/dirty_screen_four.dart
+++ lib/src/features/dirty_screen_4/presentation/dirty_screen_four.dart
@@ -1,3 +1,4 @@
+import 'package:dirty_code/src/features/dirty_screen_4/presentation/four.dart';
 import 'package:flutter/material.dart';
 
 class DirtyScreenFour extends StatelessWidget {
@@ -8,82 +9,24 @@ class DirtyScreenFour extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Dirty Screen #4"),
       ),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
           children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: Colors.blueAccent,
-              ),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.home, color: Colors.white),
-                  SizedBox(width: 8),
-                  Text(
-                    'Home',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
+            Four(
+                color: Colors.blueAccent, text: "Home", icon: Icon(Icons.home)),
+            Four(
                 color: Colors.greenAccent,
-              ),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.work, color: Colors.white),
-                  SizedBox(width: 8),
-                  Text(
-                    'Work',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
+                text: "School",
+                icon: Icon(Icons.school)),
+            Four(
                 color: Colors.orangeAccent,
-              ),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.school, color: Colors.white),
-                  SizedBox(width: 8),
-                  Text(
-                    'School',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
+                text: "Work",
+                icon: Icon(Icons.work)),
+            Four(
                 color: Colors.purpleAccent,
-              ),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.directions_bike, color: Colors.white),
-                  SizedBox(width: 8),
-                  Text(
-                    'Bike',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
+                text: "Bike",
+                icon: Icon(Icons.directions_bike)),
           ],
         ),
       ),
